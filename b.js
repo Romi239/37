c100eb3f52e5008c71f4a20ac752fab8dd7ff62c
Romy239 <rominamaximobadillo@gmail.com>
2022-01-16 16:42:24 -0600

    arrays

---
+++ b.js
@@ -0,0 +1,57 @@
+/*const listaDeNumero = [2,3,5,7,9,11];
+console.log(listaDeNumero[5])
+console.log(listaDeNumero[4])*/
+
+
+//momento de recorrer un array con un bucle FOR
+
+/*let numerosImpares = [1,3,5,7];
+cantidad = numerosImpares.length;
+console.log("Primera forma:")
+for(i = 0;cantidad > i;i++){
+    console.log(numerosImpares[i])
+}
+/* ejemplo de pares
+let numerosPares = [2,4,6,8,10];
+cantidad = numerosPares.length;
+console.log("numeros pares:")
+for(i = 0;cantidad > i;i++){
+    console.log(numerosPares[i])
+}
+
+//momento de practicar arreglos 
+//REMPLAZAR ELEMENTOS DE UN ARREGLO
+/*const listaDeNumero =[13,14,15,16,17,18];
+console.log(listaDeNumero[2])
+listaDeNumero[2]= 16814564;
+console.log(listaDeNumero[2])
+
+//AGREGAR ELEMNTOS A UN ARREGLO
+listaDeNumeros = []
+listaDeNumeros[0] = 100;
+listaDeNumeros[1] = 102;
+listaDeNumeros[2] = -11;
+listaDeNumeros[3] = 23;
+console.log(listaDeNumeros[2])
+
+//numeros y letras
+const mixedArray = [3,"hola mundo", 3627.2];
+cantidad = mixedArray.length;
+for(i = 0; cantidad > i ; i++){
+    console.log(mixedArray[i])
+}*/
+
+let questions = [
+    ['¿Cuál es la capital de Perú?','Lima'],
+    ['¿Cuántos millones de peruanos somos?','35.97'],
+    ['¿Cuál es la moneda de Perú?','Soles']
+]
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+console.log(questions[1][1]);
+
+
+
+
